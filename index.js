14db74809483c07942488a1dfdd866929f0ac997
Jwaxo <michfreak@gmail.com>
2013-12-02 16:59:37 -0800

    Added a bunch of example three.js creation stuff for later use.

--- index.js
+++ index.js
@@ -108,8 +108,39 @@ module.exports = function(grid) {
         //      where to look. Requires the fs module to be installed.
         
         this.grid = grid; //We use it in virtually every function, so assign it.
-    
-        var map = new THREE.CanvasRenderer();
+        
+        var WIDTH = 400,
+            HEIGHT = 300;
+
+        // set some camera attributes
+        var VIEW_ANGLE = 45,
+            ASPECT = WIDTH / HEIGHT,
+            NEAR = 0.1,
+            FAR = 10000;
+
+        // get the DOM element to attach to
+        // - assume we've got jQuery to hand
+        var container = document.createElement('div');
+        document.body.appendChild(container);
+
+        // create a WebGL renderer, camera
+        // and a scene
+        var renderer = new THREE.WebGLRenderer();
+        var camera = new THREE.PerspectiveCamera(  VIEW_ANGLE,
+                                        ASPECT,
+                                        NEAR,
+                                        FAR  );
+        var scene = new THREE.Scene();
+
+        // the camera starts at 0,0,0 so pull it back
+        camera.position.z = 300;
+
+        // start the renderer
+        renderer.setSize(WIDTH, HEIGHT);
+
+        // attach the render-supplied DOM element
+        container.appendChild(renderer.domElement);
+
         var thisShape = 0;
         
         for (var x=0;x<grid.length;x++) {
@@ -122,7 +153,43 @@ module.exports = function(grid) {
             
         }
     
-        return map;
+        // create the sphere's material
+        var sphereMaterial = new THREE.MeshLambertMaterial(
+        {
+            color: 0xCC0000
+        });
+
+        // set up the sphere vars
+        var radius = 50, segments = 16, rings = 16;
+
+        // create a new mesh with sphere geometry -
+        // we will cover the sphereMaterial next!
+        var sphere = new THREE.Mesh(
+           new THREE.SphereGeometry(radius, segments, rings),
+           sphereMaterial);
+
+        // add the sphere to the scene
+        scene.add(sphere);
+
+        // and the camera
+        scene.add(camera);
+
+        // create a point light
+        var pointLight = new THREE.PointLight( 0xFFFFFF );
+
+        // set its position
+        pointLight.position.x = 10;
+        pointLight.position.y = 50;
+        pointLight.position.z = 130;
+
+        // add to the scene
+        scene.add(pointLight);
+
+        // draw!
+        renderer.render(scene, camera);
+        console.log('rendered!');
+    
+        return renderer;
     }
     
     this.findShape = function(x,y,checkCorners) {
